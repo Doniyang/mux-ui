0cd48a554081e4e331ec680c779ba6a1c6748e7f
yangxd <doniyang@sina.cn>
2020-04-13 14:39:34 +0800

    组件开发

--- packages/checkbox/src/Checkbox.js
+++ packages/checkbox/src/Checkbox.js
@@ -1,73 +1,101 @@
 export default {
-	name:'v-checkbox',
-	props:{
-		value:{
-			type:Array,
-			default:()=>[]
-		},
-		name:{
-			type:String,
-			required:true
-		},
-		id:{
-			type:String,
-			default:''
-		},
-		checked: {
-            type: Boolean,
-            default: false
+  name: 'v-checkbox',
+  props: {
+    value: {
+      type: Array,
+      default: () => []
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: String,
+      default: ''
+    },
+    checked: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    partial: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    checkboxValue: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      uuid: Math.random().toString(32).substring(4, 9).toUpperCase()
+    }
+  },
+  computed: {
+    checkboxId () {
+      return this.id || 'CHECKBOX-' + this.uuid
+    }
+  },
+  methods: {
+    handleChange (e) {
+      const target = e.target || e.srcElement
+      let currentValue = [...this.value]
+      target.checked ? currentValue.push(target.value) : (currentValue = currentValue.filter(v => v !== target.value));
+      this.$emit('input', currentValue)
+    },
+    genCheckboxWrapContext () {
+      return this.$createElement('div', {
+        staticClass: 'mux-checkbox-box'
+      }, [this.genCheckboxContext()])
+    },
+    genCheckboxContext () {
+      return this.$createElement('input', {
+        staticClass: 'mux-checkbox-block',
+        class: {
+          'mux-checkbox--is-partial': this.partial
         },
-        disabled: {
-            type: Boolean,
-            default: false
-         },
-         label:{
-         	type:String,
-         	default:''
-         },
-         checkboxValue:{
-         	type:String,
-         	default:''
-         }
-	},
-    methods:{
-       genCheckboxWrapContext(){
-             return this.$createElement('div',{
-             	staticClass:'mux-checkbox-box'
-             },[this.genCheckboxContext()])
-       },
-       genCheckboxContext(){
-          return this.$createElement('input',{
-          	staticClass:'mux-checkbox-block',
-          	attrs:{
-          		type:'checkbox',
-          		name:this.name,
-          		id:this.checkboxId,
-          		checked:this.checked,
-          		disabled:this.disabled
-          	},
-          	domProps:{
-          		value:this.checkboxValue
-          	}
-          })
-       },
-       genCheckboxLabelContext(){
-          return this.$createElement('div',{
-          	staticClass:'mux-checkbox-label-wrap'
-          },[this.genLabelSlotContext()]) 
-       },
-       genLabelSlotContext(){
-          return this.$scopedSlots.label?this.$scopedSlots.label():this.$createElement('label',{
-             	staticClass:'mux-checkbox-label',
-               attrs:{
-                 for:this.radioId
-               }
-             },this.label)  
-       }
-    },  
-    render(h){
-    	return h('div',{
-    		staticClass:'component mux-checkbox'
-    	},[this.genCheckboxWrapContext(),this.genCheckboxLabelContext()])
-    } 
+        attrs: {
+          type: 'checkbox',
+          name: this.name,
+          id: this.checkboxId,
+          checked: this.checked,
+          disabled: this.disabled
+        },
+        domProps: {
+          value: this.checkboxValue
+        },
+        on: {
+          change: e => {
+            this.handleChange(e)
+          }
+        }
+      })
+    },
+    genCheckboxLabelContext () {
+      return this.$createElement('div', {
+        staticClass: 'mux-checkbox-label-wrap'
+      }, [this.genLabelSlotContext()])
+    },
+    genLabelSlotContext () {
+      return this.$scopedSlots.label ? this.$scopedSlots.label({id:this.checkboxId}) : this.$createElement('label', {
+        staticClass: 'mux-checkbox-label',
+        attrs: {
+          for: this.checkboxId
+        }
+      }, this.label)
+    }
+  },
+  render (h) {
+    return h('div', {
+      staticClass: 'component mux-checkbox'
+    }, [this.genCheckboxWrapContext(), this.genCheckboxLabelContext()])
+  }
 }
--- packages/radio-group/src/RadioGroup.js
+++ packages/radio-group/src/RadioGroup.js
@@ -1,43 +1,43 @@
 export default {
-	name:'v-radio-group',
-	props:{
-		block: {
-            type: Boolean,
-            default: false
-        },
-        value: String,
-        reverse:{
-    	    type:Boolean,
-    		default:false
-    	}
-	},
-    mounted(){
-    	this.$nextTick(()=>{
-    		this.updateChildrenNode(this.value)
-    	})
+  name: 'v-radio-group',
+  props: {
+    block: {
+      type: Boolean,
+      default: false
     },
-    watch:{
-    	value:{
-    		immediate: true,
-    		handler(nv,ov){
-    			if(nv!==ov)this.updateChildrenNode(nv)	
-    		}
-    	}
-    },  
-    methods:{
-        updateChildrenNode(value){
-        	if (this.$children.length > 0) {
-                this.$children.forEach(vm => { vm.$emit('input',value) })
-            }
-        } 
-    },   
-	render(h){
-      return h('div',{
-      	staticClass:'component mux-radio-group',
-      	class:{
-      		'mux-radio-group--is-block':this.block,
-      		'mux-radio-group--is-reverse':this.reverse
-      	}
-      },this.$slots.default) 
-	}
+    value: String,
+    reverse: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.updateChildrenNode(this.value)
+    })
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler (nv, ov) {
+        if (nv !== ov) this.updateChildrenNode(nv)
+      }
+    }
+  },
+  methods: {
+    updateChildrenNode (value) {
+      if (this.$children.length > 0) {
+        this.$children.forEach(vm => { vm.$emit('input', value) })
+      }
+    }
+  },
+  render (h) {
+    return h('div', {
+      staticClass: 'component mux-radio-group',
+      class: {
+        'mux-radio-group--is-block': this.block,
+        'mux-radio-group--is-reverse': this.reverse
+      }
+    }, this.$slots.default)
+  }
 }
