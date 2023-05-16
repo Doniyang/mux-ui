b5a85ab4c463f0903ff43c818f035fe669a0fea9
Niyang <doniyang@hotmail.com>
2023-05-16 20:22:45 +0800

    update component for p

--- packages/picker/src/Panel.js
+++ packages/picker/src/Panel.js
@@ -13,13 +13,25 @@ export default {
   },
   data(){
     return {
-      roll:null
+      roll:null,
+      selectionIndex: 0
     }
   },
   mounted(){
-     this.roll = new IRoll(this.$el.firstElementChild, {})      
+     this.roll = new IRoll(this.$el.firstElementChild, {})
+     this.roll.on("scroll:end",this.onScrollStop)
+  },
+  beforeDestroy(){
+     this.roll.off("scroll:end",this.onScrollStop)
+     this.roll.destory()
+     this.roll = null
   },
   methods:{
+     onScrollStop(e,pos){
+        let absY = Math.abs(pos.y)
+        this.selectionIndex = Math.round (absY / 44);
+        this.roll.scrollToElement('.mux-picker-roll-item[aria-current="'+this.selectionIndex+'"]',300)   
+     },
      genPanelContext(){
        return this.$createElement('div',{
           staticClass:'mux-picker-panel-wrap'
