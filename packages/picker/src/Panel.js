32224ad1ffc4a0eb892d7044272a249206d6927c
niyang <yangxd6@yusys.com.cn>
2024-03-16 17:36:42 +0800

    update Picker

--- packages/picker/src/Panel.js
+++ packages/picker/src/Panel.js
@@ -1,76 +1,82 @@
 import { IRoll } from "@niyang-es/iroll";
 export default {
-  name:'Panel',
-  props:{
-    columns:{
-      type:Array,
-      default:()=>[]
+  name: 'Panel',
+  props: {
+    columns: {
+      type: Array,
+      default: () => []
     },
-    colIndex:{
-      type:Number,
-      default:0
+    colIndex: {
+      type: Number,
+      default: 0
     },
-    alias:{
-      type:Object,
-      default:()=>({key:'key',value:'value'})
+    alias: {
+      type: Object,
+      default: () => ({ key: 'key', value: 'value' })
     },
-    selectionIndex:{
-      type:Number,
-      default:0
+    selectionIndex: {
+      type: Number,
+      default: 0
     }
   },
-  data(){
+  data() {
     return {
-      roll:null
+      roll: null
     }
   },
-  mounted(){
-     this.roll = new IRoll(this.$el.firstElementChild, {})
-     this.scrollTo(this.selectionIndex)
-     this.roll.on("scroll:end",this.onScrollStop)
+  mounted() {
+    this.roll = new IRoll(this.$el.firstElementChild, {
+      flick: false,
+      click: false,
+      tap: false,
+      scrollX: false,
+      scrollY: true
+    })
+    this.scrollTo(this.selectionIndex)
+    this.roll.on("scroll:end", this.onScrollStop)
   },
-  beforeDestroy(){
-     this.roll.off("scroll:end",this.onScrollStop)
-     this.roll.destory()
-     this.roll = null
+  beforeDestroy() {
+    this.roll.off("scroll:end", this.onScrollStop)
+    this.roll.destory()
+    this.roll = null
   },
-  methods:{
-     onScrollStop(e,pos){
-        let absY = Math.abs(pos.y)
-        let rowIndex = Math.round (absY / 44);
-        this.scrollTo(rowIndex)
-        this.$emit('columnchange',rowIndex,this.colIndex)
-     },
-     scrollTo(rowIndex){
-        this.roll.scrollTo(0, rowIndex * 44 * -1,300)  
-     },
-     genPanelContext(){
-       return this.$createElement('div',{
-          staticClass:'mux-picker-panel-wrap'
-       },[this.genPanelScrollContext()])
-     },
-     genPanelScrollContext(){
-        return this.$createElement('ul',{
-          staticClass:'mux-picker-roll',
-          attrs:{ role: 'roll' },
-          domProps: { role: 'roll' },                   
-        },this.columns.map((col,index)=>this.genPanelScrollItemContext(col,index))) 
-     },
-     genPanelScrollItemContext(column,rowIndex){
-      return this.$createElement('li',{
-        staticClass:'mux-picker-roll-item',
-        domProps:{ ariaRowIndex:  rowIndex, ariaColIndex: this.colIndex }
-      },this.updatePanelScrollContent(column))
-     },
-     updatePanelScrollContent(data){
-        return data[this.alias.value]?data[this.alias.value] : data
-     }
+  methods: {
+    onScrollStop(e, pos) {
+      let absY = Math.abs(pos.y)
+      let rowIndex = Math.round(absY / 44);
+      this.scrollTo(rowIndex)
+      this.$emit('columnchange', rowIndex, this.colIndex)
+    },
+    scrollTo(rowIndex) {
+      this.roll.scrollTo(0, rowIndex * 44 * -1, 300)
+    },
+    genPanelContext() {
+      return this.$createElement('div', {
+        staticClass: 'mux-picker-panel-wrap'
+      }, [this.genPanelScrollContext()])
+    },
+    genPanelScrollContext() {
+      return this.$createElement('ul', {
+        staticClass: 'mux-picker-roll',
+        attrs: { role: 'roll' },
+        domProps: { role: 'roll' },
+      }, this.columns.map((col, index) => this.genPanelScrollItemContext(col, index)))
+    },
+    genPanelScrollItemContext(column, rowIndex) {
+      return this.$createElement('li', {
+        staticClass: 'mux-picker-roll-item',
+        domProps: { ariaRowIndex: rowIndex, ariaColIndex: this.colIndex }
+      }, this.updatePanelScrollContent(column))
+    },
+    updatePanelScrollContent(data) {
+      return data[this.alias.value] ? data[this.alias.value] : data
+    }
   },
-  render(h){
-     return h('div',{
-      staticClass:'mux-picker-panel',
-      attrs:{role:'panel'},
-      domProps:{role:'panel'}
-     },[this.genPanelContext()])  
+  render(h) {
+    return h('div', {
+      staticClass: 'mux-picker-panel',
+      attrs: { role: 'panel' },
+      domProps: { role: 'panel' }
+    }, [this.genPanelContext()])
   }
 }
